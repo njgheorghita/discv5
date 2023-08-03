b6d9703c95a6668758abb90b9f4742bd4b9306b8
njgheorghita <nickgheorghita@gmail.com>
2023-08-03 14:02:58 -0500

    Remove expected response upon failed session

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -1282,6 +1282,7 @@ impl Handler {
                 }
             }
         }
+        self.remove_expected_response(node_address.socket_addr);
     }
 
     /// Sends a packet to the send handler to be encoded and sent.
